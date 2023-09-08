72ae1341d10b0b15f58882db0a735ff52ea58849
VB666 <vict.gamboa@duocuc.cl>
2023-09-07 23:13:34 -0300

    nuevo commit

--- RegistrAPP/src/app/registro-horario/registro-horario.page.ts
+++ RegistrAPP/src/app/registro-horario/registro-horario.page.ts
@@ -1,36 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-registro-horario',
   templateUrl: './registro-horario.page.html',
   styleUrls: ['./registro-horario.page.scss'],
 })
-export class RegistroHorarioPage implements OnInit {
+export class RegistroHorarioPage {
+  fecha: string = '';
+  dia: string = '';
+  mes: string = '';
+  hora: string = '';
+  ano: string = ''; // Agrega la variable para el año
+  
 
-  fecha:string = '';
-  dia: string ='';
-  mes: string ='';
-  ano: string ='';
-  hora: string ='';
+  constructor() { }
 
-
-  constructor() { 
-    this.mostrarFecha();
-  }
-
-  mostrarFecha() {
+  generarRegistro() {
     const fechaActual = new Date();
     this.fecha = fechaActual.toISOString();
     this.dia = this.capitalizeFirstLetter(fechaActual.toLocaleString('es-ES', { weekday: 'long' }));
     this.mes = this.capitalizeFirstLetter(fechaActual.toLocaleString('es-ES', { month: 'long' }));
-    this.ano = fechaActual.getFullYear().toString();
     this.hora = fechaActual.toLocaleTimeString();
+    this.ano = fechaActual.getFullYear().toString(); // Obtener el año
   }
 
   capitalizeFirstLetter(word: string): string {
     return word.charAt(0).toUpperCase() + word.slice(1);
   }
-  ngOnInit() {
-  }
-
 }
