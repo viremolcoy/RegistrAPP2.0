2cb340bbcd7cf62e0ee2320bef15379e14bd0981
VB666 <vict.gamboa@duocuc.cl>
2023-10-20 15:12:38 -0300

    Api Google-Map

--- RegistrAPP/src/app/map/map.page.spec.ts
+++ RegistrAPP/src/app/map/map.page.spec.ts
@@ -1,12 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AyudaPage } from './ayuda.page';
+import { MapPage } from './map.page';
 
-describe('AyudaPage', () => {
-  let component: AyudaPage;
-  let fixture: ComponentFixture<AyudaPage>;
+describe('MapPage', () => {
+  let component: MapPage;
+  let fixture: ComponentFixture<MapPage>;
 
-  beforeEach((() => {
-    fixture = TestBed.createComponent(AyudaPage);
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(MapPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
