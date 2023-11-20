d83b4ed6b7ee0c59c111dd4552c7f10a0f58efa6
viremolcoy <vi.remolcoy@duocuc.cl>
2023-11-19 21:59:40 -0300

    comitaso

--- RegistrAPP/src/app/camara/camara.page.ts
+++ RegistrAPP/src/app/camara/camara.page.ts
@@ -8,22 +8,15 @@ import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 })
 export class CamaraPage{
 
-  constructor() { }
+  constructor() {}
 
-startScan = async () => {
-  // Check camera permission
-  // This is just a simple example, check out the better checks below
-  await BarcodeScanner.checkPermission({ force: true });
+  async scanBarcode() {
+    const result = await BarcodeScanner.startScan();
 
-  // make background of WebView transparent
-  // note: if you are using ionic this might not be enough, check below
-  BarcodeScanner.hideBackground();
-
-  const result = await BarcodeScanner.startScan(); // start scanning and wait for a result
-
-  // if the result has content
-  if (result.hasContent) {
-    console.log(result.content); // log the raw scanned content
+    if (result.hasContent) {
+      alert(`Código de barras escaneado: ${result.content}`);
+    } else {
+      alert('Escaneo cancelado o sin contenido.');
+    }
   }
-};
 }
